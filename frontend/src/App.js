f92d4fc6862844b40146a7cb4bacadee841efca5
SiVv-B <siwar.blk@gmail.com>
2022-03-06 10:45:13 +0100

    appJS router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { render } from "react-dom"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"
 import ProductList from './components/ProductList'
 import Cart from './pages/Cart'
 import Home from "./pages/Home"
@@ -7,9 +14,23 @@ import Product from './pages/Product'
 import Register from './pages/Register'
 
 const App = () => {
-  return <div>
-    <Home/>
-  </div>
+  const user = true
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products/:category" element={<ProductList />} />
+            <Route path="/product/:id" element={<Product/>} />
+            <Route path="/cart" element={<Cart/>} />
+            <Route path="/login" element={user ? <Navigate to ='/'/> : <Login/>} />
+
+            <Route path="/register" element={user ? <Navigate to ='/'/> : <Register/>} />        
+          </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
